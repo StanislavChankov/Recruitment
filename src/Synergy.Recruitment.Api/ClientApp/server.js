15f24563d73429956d4b25c607fd620ab4238ae3
Stanislav Chankov <schankov@questarai.com>
2018-10-05 23:21:14 +0300

    Initial commit.

--- src/Synergy.Recruitment.Api/ClientApp/server.js
+++ src/Synergy.Recruitment.Api/ClientApp/server.js
@@ -6,10 +6,10 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/src/Synergy.Recruitment.Api/ClientApp/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/Synergy.Recruitment.Api/ClientApp/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
