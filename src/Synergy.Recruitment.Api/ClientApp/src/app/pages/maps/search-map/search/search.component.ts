a476d011e33cafc9ba2ed8ec52cb67fa05b6361a
Stanislav Chankov <schankov@questarai.com>
2018-10-25 19:03:24 +0300

    Changed the way of import googlemaps

--- src/Synergy.Recruitment.Api/ClientApp/src/app/pages/maps/search-map/search/search.component.ts
+++ src/Synergy.Recruitment.Api/ClientApp/src/app/pages/maps/search-map/search/search.component.ts
@@ -1,7 +1,8 @@
+/// <reference types="@types/googlemaps" />
+
 import { Component, ElementRef, EventEmitter, NgZone, OnInit, Output, ViewChild } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
 import { Location } from '../entity/Location';
-import {} from 'googlemaps';
 
 @Component({
   selector: 'ngx-search',
